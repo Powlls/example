0b9376c9a0a3133d71e5e1dcafb9b58a5b380db9
Ömer Polat <omerplat9@gmail.com>
2021-10-25 14:42:33 +0300

    ds

---
+++ test.sh/test1.sh
@@ -0,0 +1,25 @@
+﻿#!/bin/bash
+
+#get computer name and username
+hostname;whoami
+
+#get current working directory
+pwd
+
+#get running applications and services
+ps -aux;systemctl –state=active
+
+#get current opened ports
+ss -lnpt
+
+#get current hostname 
+hostname
+
+#get current IP and network adapters
+ip a
+
+#get available memory space
+free
+
+#get available harddisk space
+df --output=source,avail
